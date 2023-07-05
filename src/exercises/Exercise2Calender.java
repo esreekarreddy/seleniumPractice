5369922f3ab372431b9d05ef341152c024269bab
esreekarreddy <esreekarreddy@gmail.com>
2023-07-05 11:39:22 +0530

    modified code to select correct date along with year

--- src/exercises/Exercise2Calender.java
+++ src/exercises/Exercise2Calender.java
@@ -21,16 +21,17 @@ public class Exercise2Calender {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://www.globalsqa.com/demo-site/datepicker/");
 		
-		String travelDate = "December 2023";
+		String travelDate = "April 2025";
 		
 		driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[class='demo-frame lazyloaded']")));
 		//open the calender
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("p")));
 		driver.findElement(By.cssSelector("#datepicker")).click();
 		
-		while(!driver.findElement(By.cssSelector(".ui-datepicker-month")).getText().contains(travelDate.split(" ")[0])) {
+		while(!(driver.findElement(By.cssSelector(".ui-datepicker-month")).getText() + " " + driver.findElement(By.cssSelector(".ui-datepicker-year")).getText()).contains(travelDate)) {
 			driver.findElement(By.cssSelector(".ui-icon.ui-icon-circle-triangle-e")).click();
 		}
+		
 		
 		List<WebElement> days = driver.findElements(By.cssSelector("td[data-handler='selectDay']"));
 		int sizeOfDays = days.size();
